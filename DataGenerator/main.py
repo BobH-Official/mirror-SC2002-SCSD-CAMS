a8e5701f6314aefbc36933bd211cbd74a6783edb
Moon0002 <icdisw@gmail.com>
2023-10-16 23:04:21 +0800

    csv added

--- DataGenerator/main.py
+++ DataGenerator/main.py
@@ -4,7 +4,7 @@ from generators.students import generate_student_df
 from generators.staffs import generate_staff_df
 
 if __name__ == "__main__":
-    PATH = "C:/Users/o/Desktop/대학 2학년/SC2002/Assignment/SC2002-SCSD-CAMS/SC2002Project/src/files"
+    PATH = "../SC2002-SCSD-CAMS/SC2002Project/src/files"
     print("Generating files...")
 
     generate_student_df(PATH)
---
+++ DataGenerator/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+    PATH = "../SC2002-SCSD-CAMS/SC2002Project/src/files"
