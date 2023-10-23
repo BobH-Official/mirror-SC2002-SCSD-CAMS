744da140b2b6f9213826977f22a7460b9edc681d
Moon0002 <icdisw@gmail.com>
2023-10-23 22:13:43 +0800

    Create App java

---
+++ SC2002Project/src/App.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+//import package1. (add java file name);
+
+public class App{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
