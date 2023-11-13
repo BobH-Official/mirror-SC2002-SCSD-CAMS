1bdd6a6412d41b15aa9e5a810be5b771841a55e0
BobHUI <XHUI001@e.ntu.edu.sg>
2023-11-13 14:27:24 +0900

    Update RequestStatuses

--- SC2002Project/src/CAMS/Data/Request/EnquiryStatus.java
+++ SC2002Project/src/CAMS/Data/Request/EnquiryStatus.java
@@ -1,4 +1,35 @@
 package CAMS.Data;
 
 enum EnquiryStatus implements CAMS.Data.RequestStatus {
+
+  PENDING, REPLIED;
+
+  void printSelf() {
+    switch (this) {
+      case PENDING -> {
+        System.out.println("Status: PENDING");
+      }
+      case REPLIED -> {
+        System.out.println("Status: REPLIED");
+      }
+      default -> {
+        System.out.println("Status: UNKNOWN");
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    switch (this) {
+      case PENDING -> {
+        return "Status: PENDING";
+      }
+      case REPLIED -> {
+        return "Status: REPLIED";
+      }
+      default -> {
+        return "Status: UNKNOWN";
+      }
+    }
+  }
 }
--- SC2002Project/src/CAMS/Data/Request/SuggestionStatus.java
+++ SC2002Project/src/CAMS/Data/Request/SuggestionStatus.java
@@ -1,4 +1,41 @@
 package CAMS.Data;
 
 enum SuggestionStatus implements CAMS.Data.RequestStatus {
+
+  PENDING, APPROVED, REJECTED;
+
+  void printSelf() {
+    switch (this) {
+      case PENDING -> {
+        System.out.println("Status: PENDING");
+      }
+      case APPROVED -> {
+        System.out.println("Status: APPROVED");
+      }
+      case REJECTED -> {
+        System.out.println("Status: REJECTED");
+      }
+      default -> {
+        System.out.println("Status: UNKNOWN");
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    switch (this) {
+      case PENDING -> {
+        return "Status: PENDING";
+      }
+      case APPROVED -> {
+        return "Status: APPROVED";
+      }
+      case REJECTED -> {
+        return "Status: REJECTED";
+      }
+      default -> {
+        return "Status: UNKNOWN";
+      }
+    }
+  }
 }
