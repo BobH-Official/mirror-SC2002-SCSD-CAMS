7f21f23df3855f9d5fa7e774187545d7d3486394
jeeho02 <jiho0709.lee@gmail.com>
2023-11-15 01:00:04 +0800

    added 1st feedback

--- SC2002Project/src/CAMS/Data/User/StaffData.java
+++ SC2002Project/src/CAMS/Data/User/StaffData.java
@@ -1,6 +1,43 @@
 package CAMS.Data;
 
-class StaffData extends CAMS.Data.UserData {
+import java.util.List;
+import java.util.ArrayList;
 
-  // TODO: functions should be `protected`
+class StaffData extends UserData {
+    private List<String> campsUnderManagement;
+
+    // Constructor
+    StaffData(String name, String email, String faculty, List<String> campsUnderManagement) {
+        super(name, email, faculty);
+        this.campsUnderManagement = new ArrayList<>(campsUnderManagement);
+    }
+
+    // Methods
+    boolean isinChargeOf(String campID) {
+        return campsUnderManagement.contains(campID);
+    }
+
+    void addCampToManagement(String campID) {
+        if (!isinChargeOf(campID)) {
+            campsUnderManagement.add(campID);
+        }
+    }
+
+    void removeCampFromManagement(String campID) {
+        campsUnderManagement.remove(campID);
+    }
+
+    // Getters
+    List<String> getCampsUnderManagement() {
+        return new ArrayList<>(campsUnderManagement); // Return a copy of the list
+    }
+
+    void printSelf() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString() + "Camps under management: " + campsUnderManagement.toString()).indent(4);
+    }
 }
--- SC2002Project/src/CAMS/Data/User/StudentData.java
+++ SC2002Project/src/CAMS/Data/User/StudentData.java
@@ -1,30 +1,102 @@
 package CAMS.Data;
 
-class StudentData extends CAMS.Data.UserData {
+import java.util.ArrayList;
+import java.util.List;
 
-  private final String userID;
-  private String password;
+public class StudentData extends UserData {
+    private List<CampData> campAttendee;
+    private List<String> ownEnquiry; // Assuming integer IDs for enquiries
+    private DataRangeList datesOccupied; // Assuming DataRangeList is a defined class
+    private boolean isCommitteeMember;
+    private String campAsCommitteeMember; // Assuming camp ID is a string
+    private int pointsForGivingSuggestions;
+    private int pointsForApprovedSuggestion;
+    private int pointsForReplyingEnquiry;
+    private List<String> ownSuggestions; // Assuming integer IDs for suggestions
 
-  StudentData(String id, String passwd) {
-    this.userID = id;
-    this.password = passwd;
-  }
+    public StudentData(String name, String email, String faculty) {
+        super.createUser(name, email, faculty);
+        this.campAttendee = new ArrayList<>();
+        this.ownEnquiry = new ArrayList<>();
+        this.datesOccupied = new DataRangeList(); // Assuming default constructor
+        this.isCommitteeMember = false;
+        this.campAsCommitteeMember = "";
+        this.pointsForGivingSuggestions = 0;
+        this.pointsForApprovedSuggestion = 0;
+        this.pointsForReplyingEnquiry = 0;
+        this.ownSuggestions = new ArrayList<>();
+    }
 
+    public boolean isCommitteeMember() {
+        return isCommitteeMember;
+    }
 
-  // TODO: functions should be `package-level private`, ie no annotation
-  void setPassword(String passwd) {
-    this.password = passwd;
-  }
+    public CampData getCampAsCommitteeMember() {
+        // Assuming there's a class CampManager with a method findCampById
+        return CampManager.findCampById(this.campAsCommitteeMember);
+    }
+    
 
-  void printSelf() {
-    System.out.println("STUDENT  : ");
-    System.out.println("  ID     : " + this.userID);
-    System.out.println("  PASSWD : " + this.password);
-  }
+    public void increasePointsForGivingSuggestions() {
+        this.pointsForGivingSuggestions++;
+    }
 
-  public String toString() {
-    //    printSelf()
-    return "STUDENT  : \n  ID     : " + this.userID + "\n  PASSWD : " +
-      this.password;
-  }
+    public void increasePointsForApprovedSuggestions() {
+        this.pointsForApprovedSuggestion++;
+    }
+
+    public void increasePointsForReplyingEnquiry() {
+        this.pointsForReplyingEnquiry++;
+    }
+
+    public int getPointsForGivingSuggestions() {
+        return pointsForGivingSuggestions;
+    }
+
+    public int getPointsForApprovedSuggestions() {
+        return pointsForApprovedSuggestion;
+    }
+
+    public int getPointsForReplyingEnquiry() {
+        return pointsForReplyingEnquiry;
+    }
+
+    public List<CampData> getCampAsAttendee() {
+        return campAttendee;
+    }
+
+    public List<Integer> getOwnEnquiries() {
+        return ownEnquiry;
+    }
+
+    public DataRangeList getDatesOccupied() {
+        return datesOccupied;
+    }
+
+    public List<Integer> getOwnSuggestions() {
+        return ownSuggestions;
+    }
+
+    public void joinAsAttendeeOf(CampData camp) {
+        this.campAttendee.add(camp);
+    }
+
+    public void withdrawFrom(CampData camp) {
+        this.campAttendee.remove(camp);
+    }
+
+    public void addEnquiry(int enquiryId) {
+        this.ownEnquiry.add(enquiryId);
+    }
+
+    public void addSuggestion(int suggestionId) {
+        this.ownSuggestions.add(suggestionId);
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return (super.toString() + "Committee Member: " + isCommitteeMember).indent(4);
+    }
 }
