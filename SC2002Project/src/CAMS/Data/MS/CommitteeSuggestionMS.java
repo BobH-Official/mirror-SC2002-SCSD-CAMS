5de633cac6d771c39c0646e99428fee9847faf77
Moon0002 <icdisw@gmail.com>
2023-11-15 16:08:04 +0800

    Suggestion MS (Wrote some current problems of our uml in the comments in the code)

--- SC2002Project/src/CAMS/Data/MS/CommitteeSuggestionMS.java
+++ SC2002Project/src/CAMS/Data/MS/CommitteeSuggestionMS.java
@@ -1,4 +1,67 @@
 package CAMS.Data;
 
 public class CommitteeSuggestionMS {
+    private String userID;
+
+    // Constructor
+    public CommitteeSuggestionMS(String userID) {
+        this.userID = userID;
+    }
+
+    // Method to submit a suggestion and return the suggestion ID
+    public String submitSuggestion(String suggestionDescription) {
+        // Create a new suggestion data object and add it to the suggestion map in Database
+        Database.createSuggestion(this.userID, suggestionDescription, "Default Camp");
+        //made temproary module getLastInsertedSuggestionID cuz there's no way to obtain the ID
+        // of the suggestion cuz the database doesn't return the ID in any circumstances
+        // but accoridng to the ULM, this module is supposed to return the ID.
+        String suggestionID = Database.getLastInsertedSuggestionID();
+        return suggestionID;
+    }
+
+    // Method to view own suggestion details
+    public int viewOwnSuggestion(String suggestionID) {
+        // Retrieve the suggestion using its ID
+        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+        // Check if the suggestion exists and belongs to the user
+        if (suggestion != null && suggestion.sender().equals(this.userID)) {
+            System.out.println("Suggestion Details: " + suggestion);
+            return 0; // success
+        } else {
+            System.out.println("Suggestion not found or access denied.");
+            return 1; // failure or no permission
+        }
+    }
+
+    // Method to edit own suggestion
+    public int editOwnSuggestion(String suggestionID, String newMessage) {
+        // Retrieve the suggestion using its ID
+        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+        // Check if the suggestion exists and belongs to the user
+        if (suggestion != null && suggestion.sender().equals(this.userID)) {
+            suggestion.setMessage(newMessage);
+            Database.updateSuggestion(suggestionID, suggestion);
+            return 0; // success
+        } else {
+            System.out.println("Suggestion not found or access denied.");
+            return 1; // failure or no permission
+        }
+    }
+
+    // Method to delete own suggestion
+    public int deleteOwnSuggestion(String suggestionID) {
+        // Retrieve the suggestion using its ID
+        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+        // Check if the suggestion exists and belongs to the user
+        if (suggestion != null && suggestion.sender().equals(this.userID)) {
+            // made temporary module Database.deleteSuggestion exists to delete the suggestion
+            // there's only deleteEnuiry in the Student operator
+            //nothing about deleting suggestion in Student or Committee Operator
+            Database.deleteSuggestion(suggestionID);
+            return 0; // success
+        } else {
+            System.out.println("Suggestion not found or access denied.");
+            return 1; // failure or no permission
+        }
+    }
 }
--- SC2002Project/src/CAMS/Data/MS/StaffSuggestionMS.java
+++ SC2002Project/src/CAMS/Data/MS/StaffSuggestionMS.java
@@ -1,4 +1,43 @@
 package CAMS.Data;
 
 public class StaffSuggestionMS {
+    private String userID;
+
+    // Constructor
+    public StaffSuggestionMS(String userID) {
+        this.userID = userID;
+    }
+
+    // Method to view details of a Suggestion
+    public void viewSuggestion(String suggestionID) {
+        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+        if (suggestion != null) {
+            System.out.println("Suggestion Details: " + suggestion);
+        } else {
+            System.out.println("Suggestion not found for ID: " + suggestionID);
+        }
+    }
+
+    // Method to approve a Suggestion
+    public void approveSuggestion(String suggestionID) {
+        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+        if (suggestion != null) {
+            suggestion.approve();
+            System.out.println("Suggestion approved for ID: " + suggestionID);
+        } else {
+            System.out.println("Suggestion not found for ID: " + suggestionID);
+        }
+    }
+
+    // Method to reject a Suggestion
+    public void rejectSuggestion(String suggestionID) {
+        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+        if (suggestion != null) {
+            suggestion.reject();
+            System.out.println("Suggestion rejected for ID: " + suggestionID);
+        } else {
+            System.out.println("Suggestion not found for ID: " + suggestionID);
+        }
+    }
 }
+
