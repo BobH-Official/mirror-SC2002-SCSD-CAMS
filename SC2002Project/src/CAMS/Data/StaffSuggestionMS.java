f7b836e93f1d788005250124a32d08a95735a95d
BobHUI <XHUI001@e.ntu.edu.sg>
2023-11-19 21:54:27 +0900

    update suggestionMS

--- SC2002Project/src/CAMS/Data/StaffSuggestionMS.java
+++ SC2002Project/src/CAMS/Data/StaffSuggestionMS.java
@@ -1,40 +1,78 @@
 package CAMS.Data;
 
+import java.io.Console;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class StaffSuggestionMS {
-    private String userID;
+  private final String userID;
 
-    // Constructor
-    public StaffSuggestionMS(String id) {
-        this.userID = id;
+  // Constructor
+  public StaffSuggestionMS(String id) {
+    this.userID = id;
+  }
+
+  // Method to view details of a Suggestion
+  public void viewSuggestion() {
+    System.out.println("\n## VIEW SUGGESTION ##\n");
+    Console console = System.console();
+    String camp = console.readLine(STR."""
+      Which camp's suggestion do you want to view? (leave blank to print all the camp for your faculty)
+      Enter camp name:\s""").strip();
+
+    while (camp.isEmpty()) {
+      Database.printCampsForStaff(userID);
+      camp = console.readLine(STR."""
+        Which camp's suggestion do you want to view? (leave blank to print all the camp for your faculty)
+        Enter camp name:\s""").strip();
+    }
+    System.out.println(STR. "These are all messages for the camp: \{ camp }" );
+    List<RequestData<SuggestionStatus>> suggestions =
+      Database.getCampRequestList(camp, Database.RequestType.SUGGESTION);
+
+    for (int i = 0; i < suggestions.size(); i += 1) {
+      System.out.print(STR. "    \{ i }. " );
+      System.out.println(
+        suggestions.get(i).toString().indent(4).stripLeading());
+    }
+  }
+
+  // Method to approve a Suggestion
+  public void suggestionActions() {
+
+
+    // 1. you view the suggestions
+    // copy the code in viewSuggestions, you will have a List
+
+    // 2. you get the ID of the suggestion, input is a string
+
+    String choice = "1";
+
+    while (isNotNumeric(choice)) {
+      // ask for input again
+
+      choice = "2";
     }
 
-    // Method to view details of a Suggestion
-    public boolean viewSuggestion(String suggestionID) {
-        SuggestionData suggestion = Database.findSuggestion(suggestionID);
-        if (suggestion == null) {
-            return false;
-        }
-        System.out.println("Suggestion Details: " + suggestion);
-        return true;
-    }
 
-    // Method to approve a Suggestion
-    public void approveSuggestion(String suggestionID) {
-        SuggestionData suggestion = Database.findSuggestion(suggestionID);
-        if (suggestion == null) {
-            return;
-        }
-        suggestion.approve();
-        System.out.println("Suggestion approved for ID: " + suggestionID);
-    }
+    // 3. you ask for action
 
-    // Method to reject a Suggestion
-    public void rejectSuggestion(String suggestionID) {
-        SuggestionData suggestion = Database.findSuggestion(suggestionID);
-        if (suggestion == null) {
-            return;
-        }
-        suggestion.reject();
-        System.out.println("Suggestion rejected for ID: " + suggestionID);
+    // 4. you preform the action
+//        SuggestionData suggestion = Database.findSuggestion(suggestionID);
+//        if (suggestion == null) {
+//            return;
+//        }
+//        suggestion.approve();
+//        System.out.println("Suggestion approved for ID: " + suggestionID);
+  }
+
+  boolean isNotNumeric(String strNum) {
+    Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+    if (strNum == null) {
+      return true;
     }
+    return !pattern.matcher(strNum).matches();
+  }
+
+
 }
