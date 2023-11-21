c4972b8697c369548eb49dd3d3619045f942d90c
EugeneIanTeoAldridge <131367367+EugeneIanTeoAldridge@users.noreply.github.com>
2023-11-21 18:39:48 +0800

    Update ManagerEnquiryMS.java

--- SC2002Project/src/CAMS/Data/ManagerEnquiryMS.java
+++ SC2002Project/src/CAMS/Data/ManagerEnquiryMS.java
@@ -25,11 +25,58 @@ public class ManagerEnquiryMS {
 
   }
 
-  public void view() {
-    // 1. ask for which camp to view
+  public void viewEnquiry() {  
+    //check to see if its a CM or a staff
+    if (Database.finduser() == StudentData){
+      //if user is CM then continue with this branch
+      if (StudentData.isCommitteeMember()){
+        UserData committeeMember = Database.finduser(); 
+        //since CM only can view enquiries from attendees for the camp they are in charge of
+        //and they can only be CM for one camp, simply get all the enquiries from the camp they 
+        //are incharge of       
+        String camp = committeeMember.campCommittee();
+        //get all the enquiries from the camp that CM is in and print them out
+        System.out.println(STR. "These are all messages for the camp: \{ camp }" );
+        List<RequestData<EnquiryStatus>> enquiries =
+        Database.getCampRequestList(camp, Database.RequestType.ENQUIRY);
 
-    // 2. get user input
+        for (int i = 0; i < enquiries.size(); i += 1) {
+          System.out.print(STR. "    \{ i }. " );
+          System.out.println(
+            suggestions.get(i).toString().indent(4).stripLeading());
+        }
+      }
+    }
+    //if user is Staff then continue with this branch
+    else if (Database.finduser() = StaffData){
+      //since staff can manage multiple camp prompt the staff what camp they want to choose to see the enquiries
+      System.out.println("\n## VIEW SUGGESTION ##\n");
+      Console console = System.console();
+      String camp = console.readLine(STR."""
+        Which camp's enquiries do you want to view? (leave blank to print all the camp you manage)
+        Enter camp name:\s""").strip();
 
-    // 3. print all enquiries.
-  }
+      while (camp.isEmpty()) {
+        Database.printCampsForStaff(userID);
+        camp = console.readLine(STR."""
+        Which camp's enquiries do you want to view? (leave blank to print all the camp you manage)
+        Enter camp name:\s""").strip();
+
+        //get the list of all enquiries from the choosen camp and then print them out
+        System.out.println(STR. "These are all messages for the camp: \{ camp }" );
+        List<RequestData<EnquiryStatus>> enquiries =
+        Database.getCampRequestList(camp, Database.RequestType.ENQUIRY);
+
+        for (int i = 0; i < enquiries.size(); i += 1) {
+          System.out.print(STR. "    \{ i }. " );
+          System.out.println(
+            suggestions.get(i).toString().indent(4).stripLeading());
+        }
+      }
+    }
+    //return if user is not a cm or staff
+    else{
+      return;
+    }
+  }  
 }
